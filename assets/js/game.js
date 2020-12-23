3ec29e420a9d5cf7a3858ffb6ae0dee78ca9730d
Count Zero <joelecox1@gmail.com>
2020-12-22 20:57:02 -0600

    code snapshot

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,19 +1,26 @@
+// var playerName = 'Clank McKrank';
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
+console.log(playerName);
+console.log(playerHealth, playerAttack, playerMoney);
+console.log("You can put regular sentences in these.");
+
 var enemyName = "Roborto";
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function () {
+// fight function
+var fight = function() {
   // Alert players that they are starting the round
   window.alert("Welcome to Robot Gladiators!");
 
-  var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
+  // ask player if they'd like to fight or run
+  var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
 
-  // if player choses to fight, then fight
+  // if player choses to fight, fight
   if (promptFight === "fight" || promptFight === "FIGHT") {
     // remove enemy's health by subtracting the amount set in the playerAttack variable
     enemyHealth = enemyHealth - playerAttack;
@@ -28,7 +35,7 @@ var fight = function () {
       window.alert(enemyName + " still has " + enemyHealth + " health left.");
     }
 
-    // remove player's health by subtracting the amount set in the enemyAttack variable
+    // remove players's health by subtracting the amount set in the enemyAttack variable
     playerHealth = playerHealth - enemyAttack;
     console.log(
       enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
@@ -54,10 +61,12 @@ var fight = function () {
     // if no (false), ask question again by running fight() again
     else {
       fight();
-    }    
+    }
+    // if player did not chose 1 or 2 in prompt
   } else {
-    window.alert("You need to choose a valid option. Try again!");
+    window.alert("You need to pick a valid option. Try again!");
   }
-}
+};
 
+// run fight function to start game
 fight();
