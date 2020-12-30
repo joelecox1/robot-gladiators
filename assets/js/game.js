80c72302c1daba838178add7a5599ba421ca0883
Count Zero <joelecox1@gmail.com>
2020-12-30 15:48:16 -0600

    broke code had to redo

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,22 +1,19 @@
-// var playerName = 'Clank McKrank';
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-var enemyNames = ["Roborto", "Amy Android", "Robo Trummble"]
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-// fight function
-var fight = function(enemyName) {
+var fight = function (enemyName) {
   // Alert players that they are starting the round
   window.alert("Welcome to Robot Gladiators!");
 
-  // ask player if they'd like to fight or run
-  var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
+  var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
-  // if player choses to fight, fight
+  // if player choses to fight, then fight
   if (promptFight === "fight" || promptFight === "FIGHT") {
     // remove enemy's health by subtracting the amount set in the playerAttack variable
     enemyHealth = enemyHealth - playerAttack;
@@ -31,7 +28,7 @@ var fight = function(enemyName) {
       window.alert(enemyName + " still has " + enemyHealth + " health left.");
     }
 
-    // remove players's health by subtracting the amount set in the enemyAttack variable
+    // remove player's health by subtracting the amount set in the enemyAttack variable
     playerHealth = playerHealth - enemyAttack;
     console.log(
       enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
@@ -58,13 +55,11 @@ var fight = function(enemyName) {
     else {
       fight();
     }
-    // if player did not chose 1 or 2 in prompt
   } else {
-    window.alert("You need to pick a valid option. Try again!");
+    window.alert("You need to choose a valid option. Try again!");
   }
 };
 
 for(var i = 0; i < enemyNames.length; i++) {
   fight(enemyNames[i]);
 }
-
